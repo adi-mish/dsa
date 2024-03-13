901fcf9eb2d6d9e58e976964ccf20c3370d8f9a4
adi-mish <adimishra545@gmail.com>
2024-03-12 23:05:35 -0400

    minor edit :)

--- lc_practice/medium/q2.cpp
+++ lc_practice/medium/q2.cpp
@@ -18,8 +18,8 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        long num1 = 0;
-        long num2 = 0;
+        long long num1 = 0;
+        long long num2 = 0;
         while (l1) {
             num1 = num1 * 10 + l1->val;
             l1 = l1->next;

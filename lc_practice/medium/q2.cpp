306c2e0328c7f3de44ea44f4f3b2dc523fff7611
adi-mish <adimishra545@gmail.com>
2024-03-12 23:02:32 -0400

    medium problem :)

--- lc_practice/medium/q2.cpp
+++ lc_practice/medium/q2.cpp
@@ -18,6 +18,53 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        long num1 = 0;
+        long num2 = 0;
+        while (l1) {
+            num1 = num1 * 10 + l1->val;
+            l1 = l1->next;
+        }
+        while (l2) {
+            num2 = num2 * 10 + l2->val;
+            l2 = l2->next;
+        }
+        long long sum = num1 + num2;
+        ListNode *head = nullptr;
+        ListNode *tail = nullptr;
+        do {
+            ListNode *node = new ListNode(sum % 10);
+            if (!head) {
+                head = tail = node;
+            } else {
+                tail->next = node;
+                tail = node;
+            }
+            sum /= 10;
+        } while (sum > 0);
+        return head;
     }
 };
+
+int main() {
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+
+    while (result) {
+        cout << result->val;
+        result = result->next;
+        if (result) {
+            cout << "->";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
